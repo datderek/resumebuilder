033100389355f07ca38132b8dd685ae30b3f6cf4
Derek Tran <tran.derek.d.@gmail.com>
2024-04-24 18:50:49 -0700

    Add style to Panel

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,9 @@
+import '../styles/Input.css';
+
 export default function Input({ label, type='text', onChange }) {
   return (
     <label>
-      {label}:
+      <div className='label-name'>{label}:</div>
       <input type={type} onChange={onChange} />
     </label>
   )
--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -4,10 +4,18 @@ import '../styles/Panel.css';
 export default function Panel({ label, children }) {
   const [isActive, setIsActive] = useState(false);
 
+  const handleClick = (e) => {
+    setIsActive(!isActive);
+  }
+
   return (
-    <div className="panel">
-      <div>{label}</div>
-      {children}
+    <div className='panel'>
+      <div className={'panel-tab' + (isActive ? ' active' : '')} onClick={handleClick}>{label}</div>
+      {isActive && (
+        <div className='inputs'>
+          {children}
+        </div>
+      )}
     </div>
   )
 }
